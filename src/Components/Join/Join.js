622d2ec9bddb67de55c6e50a2ccf78c8407087a7
Mamednoor <mamed.gohabur@gmail.com>
2020-06-25 12:19:39 +0200

    change propertie

--- src/Components/Join/Join.js
+++ src/Components/Join/Join.js
@@ -7,32 +7,40 @@ export default function SignIn() {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
 
+  const getRandomInt = () => {
+    return Math.floor(Math.random() * Math.floor(100000))
+  }
+
   return (
     <div className="joinOuterContainer">
       <div className="joinInnerContainer">
-        <h1 className="heading">Join</h1>
+        <h1 className="heading">Contact</h1>
         <div>
           <input
             placeholder="Name"
             className="joinInput"
             type="text"
-            onChange={(event) => setName(event.target.value)}
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
-        <div>
-          <input
-            placeholder="Room"
-            className="joinInput mt-20"
-            type="text"
-            onChange={(event) => setRoom(event.target.value)}
-          />
+
+        <div className="JoinSelect mt-20">
+          <select onChange={(e) => setRoom(e.target.value + '#' + getRandomInt())} className="joinInput" name="room" id="room">
+            <option className="joinInput" value="Headach">Headach</option>
+            <option className="joinInput" value="Pregnant">Pregnant</option>
+            <option className="joinInput" value="Follow">Follow</option>
+            <option className="joinInput" value="Ordonnance">Ordonnance</option>
+            <option className="joinInput" value="Exemple">Exemple</option>
+            <option className="joinInput" value="Exemple">Exemple</option>
+          </select>
         </div>
+      
         <Link
           onClick={(e) => (!name || !room ? e.preventDefault() : null)}
           to={`/chat?name=${name}&room=${room}`}
         >
           <button className={"button mt-20"} type="submit">
-            Sign In
+            Call Doctor
           </button>
         </Link>
       </div>
