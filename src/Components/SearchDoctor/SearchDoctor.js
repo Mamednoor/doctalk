f2b9230278b7e5b366587a0e5410d6943293cd5a
Mamednoor <mamed.gohabur@gmail.com>
2020-06-25 12:52:41 +0200

    reorganise

--- src/Components/SearchDoctor/SearchDoctor.js
+++ src/Components/SearchDoctor/SearchDoctor.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 
 import './SearchDoctor.css'
-import CardDoctor from './CardDoctor';
+import CardDoctor from '../CardDoctor/CardDoctor';
 
 function SearchDoctor(props) {
     const [professions, setProfessions] = useState([])
