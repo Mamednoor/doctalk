377a5ac54333d264afa5fb5563bb90399db2a3ba
Auxence <auxence.blondel@gmail.com>
2020-06-26 11:35:56 +0200

    message box connect back

--- src/Components/register/register.js
+++ src/Components/register/register.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Route, NavLink, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, NavLink} from 'react-router-dom'
 import patientRegister from './patientRegister'
 import medecinRegister from './medecinRegister'
 import Logo from "../shares/dokitalk.png";
-
+import Login from "../login/login"
 import "./register.css";
 
 const Register = () => {
@@ -24,17 +24,14 @@ const Register = () => {
               Patient
             </NavLink>
             <NavLink
-              exact
               to='/medecinRegister'
               className="register-navlink"
             >
               Doctor
             </NavLink>
+          <a className="link-home-register" href="/">Back Home</a>
           </div>
-          <Link className="link-home-register" to="/">
-            <p className="p-home-register">Back home</p>
-          </Link>
-          <Route exact path='/patientRegister' component={patientRegister} />
+          <Route path='/patientRegister' component={patientRegister} />
           <Route path='/medecinRegister' component={medecinRegister} />
         </div>
       </div>
